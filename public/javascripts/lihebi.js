6b1e3234f30d2fc07d206e8795070c4499d99064
Hebi <lihebi@mail.ustc.edu.cn>
2014-03-14 13:54:55 +0800

    auto commit

--- public/javascripts/lihebi.js
+++ public/javascripts/lihebi.js
@@ -1,92 +1,14 @@
 $(document).ready(function(){
-    // easing
-    $.easing.def = "easeOutBack";
-    $("#logo").animate({left: '120px'}, 1000); //若用320px，必须加引号。320则不用
-    
-    //slide show 3 image
-    var $stopall = function(){
-	$('#zijiaimg').stop(false, true);
-	$('#shangwuimg').stop(false, true);
-	$('#hunqingimg').stop(false, true);
-    };
-    $('#zijia').mouseenter(function(){
-	$stopall();
-	$('#zijiaimg').fadeIn();
-	$('#zijia').addClass('current');
-    }).mouseleave(function(){
-	$('#zijiaimg').fadeOut();
-	$('#zijia').removeClass('current');
+    $('#regbtn').click(function(){
+	$('#logintabbtn').removeClass('active');
+	$('#regtabbtn').addClass('active');
+	$('#logintab').removeClass('active');
+	$('#regtab').addClass('active');
     });
-    $('#shangwu').mouseenter(function(){
-	$stopall();
-	$('#shangwuimg').fadeIn();
-	$('#shangwu').addClass('current');
-    }).mouseleave(function(){
-	$('#shangwuimg').fadeOut();
-	$('#shangwu').removeClass('current');
-    });
-    $('#hunqing').mouseenter(function(){
-	$stopall();
-	$('#hunqingimg').fadeIn();
-	$('#hunqing').addClass('current');
-    }).mouseleave(function(){
-	$('#hunqingimg').fadeOut();
-	$('#hunqing').removeClass('current');
-    });
-
-
-    // smooth scroll to 3 sections
-    $('#zijia').click(function(){
-	$.smoothScroll({scrollTarget: '#second', speed: 400});
-    });
-    $('#shangwu').click(function(){
-	$.smoothScroll({scrollTarget: '#third', speed: 400});
-    });
-    $('#hunqing').click(function(){
-	$.smoothScroll({scrollTarget: '#fourth', speed: 400});
-    });
-    $('#sectionmenu a').smoothScroll();
-    slyoperation(0);
-    slyoperation(1);
-    slyoperation(2);
-    $(window).scroll(function(){
-	if ($(document).scrollTop()>200) {
-	    $('#header').addClass('small');
-	    $('#logo').addClass('small');
-	}
-	if ($(document).scrollTop()<200) {
-	    $('#header').removeClass('small');
-	    $('#logo').removeClass('small');
-	}
-    });
-
-    //login
-    $('#login').click(function(){
-	$('#loginarea').modal();
+    $('#loginbtn').click(function(){
+	$('#regtabbtn').removeClass('active');
+	$('#logintabbtn').addClass('active');
+	$('#regtab').removeClass('active');
+	$('#logintab').addClass('active');
     });
 });
-
-function slyoperation(i) {
-    var $frame = $('.slyframe').eq(i);
-    var $slide = $frame.children('ul').eq(0);
-    var $wrap = $frame.parent();
-    $frame.sly({
-	horizontal: 1,
-	itemNav: 'basic',
-	smart: 1,
-	activateOn: 'click',
-	mouseDragging: 1,
-	releaseSwing: 1,
-	startAt: 3,
-	scrollBar: $wrap.find('.slyscrollbar'),
-	//scrollBy: 1,
-	pagesBar: $wrap.find('.slypages'),
-	activatePageOn: 'click',
-	speed: 300,
-	elasticBounds: 1,
-	easing: 'easeOutExpo',
-	drapHandle: 1,
-	dynamicHandle: 1,
-	clickBar: 1
-    });
-};
